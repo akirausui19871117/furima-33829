aa5e2463b660cfc61de339f90b0b83c08cf360e7
akirausui19871117 <akirausui19871117@users.noreply.github.com>
2021-03-21 13:07:32 +0900

    修正その４
    正規表現の修正
    不要な「unique:true」の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :email,                 uniqueness: true
-         validates :password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: "Include both letters and numbers" }
+         validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
          with_options presence: true do
            validates :nickname
            validates :birthday
-           with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"} do
+           with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"} do
             validates :family_name 
             validates :first_name
            end
