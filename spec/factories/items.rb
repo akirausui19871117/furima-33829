e291f8870432561aeea21742f5ffe04a60a2ad75
akirausui19871117 <akirausui19871117@users.noreply.github.com>
2021-03-29 19:09:46 +0900

    テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   { Faker::Name.initials(number: 6) }
+    explanation            { Faker::Lorem.sentence}
+    price                  { "10000" }
+    category_id            { "2" }
+    condition_id           { "2" }
+    delivery_fee_id        { "2" }
+    prefecture_id          { "2" }
+    delivery_day_id        { "2" } 
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品機能' do
+    context '出品ができる場合' do
+      it 'すべてのデータが入力されていればできる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できない場合' do
+      it 'nameが空では投稿できない' do
+        @item.name = ' '
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '画像が空では投稿できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'explanationが空では投稿できない' do
+        @item.explanation = ' '
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      it 'category_idが--では投稿できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+
+      end
+
+      it 'condition_idが--では投稿できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition Select")
+
+      end
+
+      it 'delivery_fee_idが--では投稿できない' do
+        @item.delivery_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee Select")
+      end
+
+      it 'prefecture_idが--では投稿できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture Select")
+      end
+
+      it 'delivery_day_idが--では投稿できない' do
+        @item.delivery_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day Select")
+      end
+
+    end
+  end
 end
