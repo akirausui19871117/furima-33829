0faaf7683883d82c8e1e400ec770c9b41824889c
akirausui19871117 <akirausui19871117@users.noreply.github.com>
2021-04-04 18:50:31 +0900

    商品削除機能の実装
    ルーティング〜アクションコントローラーの設定
    ビューの設定（削除ボタンの実装）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create,:update]
-  before_action :set_item          , only: [:edit,:update]
-  before_action :move_to_index     , only: [:edit,:update]
+  before_action :set_item          , only: [:edit,:update,:destroy]
+  before_action :move_to_index     , only: [:edit,:update,:destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -16,6 +16,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+  
+  def destroy
+    
+    @item.destroy
+    redirect_to root_path
   end
 
   def update
